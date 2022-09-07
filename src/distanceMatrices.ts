aca2b837a9c306aab28365d8b5bd054026729362
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-09-07 17:50:57 +0100

    Tidy up

--- src/distanceMatrices.ts
+++ src/distanceMatrices.ts
@@ -30,11 +30,48 @@ function adjustPoint(
   }
 }
 
-function calcCenterOffset(point: Point, boundsCenter: Point): Point {
+/**
+ * Calculate the offset from the target point to the center
+ * in order to shift the other points around it.
+ *
+ * @param targetPoint The target point.
+ * @param rectangleCenter The center of the target point in the rectangle.
+ * @returns The offset.
+ */
+function calcCenterOffset(targetPoint: Point, rectangleCenter: Point): Point {
   return {
-    x: boundsCenter.x - point.x,
-    y: boundsCenter.y - point.y
+    x: rectangleCenter.x - targetPoint.x,
+    y: rectangleCenter.y - targetPoint.y
   }
+}
+
+/**
+ * Calculate the center of the rectangle, shifted such that the
+ * top left is at x=0, y=0.
+ *
+ * @param rectangle A rectangle
+ * @returns A point representing the center of the rectangle.
+ */
+function calcRectangleCenter(rectangle: Rectangle): Point {
+  return {
+    x: -rectangle.x + rectangle.width / 2,
+    y: -rectangle.y + rectangle.height / 2
+  }
+}
+
+/**
+ * Create a square matrix to hold the distance between points.
+ *
+ * @param count The number of points.
+ * @returns A matrix of index and distance where distance is NaN.
+ */
+function prepareDistanceMatrix(count: number): DistancePoint[][] {
+  return Array.from(new Array(count), () =>
+    Array.from(Array(count), (a, i) => ({
+      index: i,
+      distance: Number.NaN
+    }))
+  )
 }
 
 /**
@@ -52,28 +89,18 @@ export function generateDistanceMatrix<T>(
   rectangle: Rectangle,
   calcDistance: (a: Point, b: Point) => number
 ): DistancePoint[][] {
-  const boundsCenter = {
-    x: -rectangle.x + rectangle.width / 2,
-    y: -rectangle.y + rectangle.height / 2
-  }
-
-  // A square matrix to hold the distance between points.
-  const m = Array.from(new Array(data.length), () =>
-    Array.from(Array(data.length), (a, i) => ({
-      index: i,
-      distance: Number.NaN
-    }))
-  )
+  const rectangleCenter = calcRectangleCenter(rectangle)
+  const m = prepareDistanceMatrix(data.length)
 
   // Calculate the distance between all the points.
   for (let i = 0; i < data.length; ++i) {
-    const centerOffset = calcCenterOffset(getPoint(data[i]), boundsCenter)
+    const centerOffset = calcCenterOffset(getPoint(data[i]), rectangleCenter)
 
     for (let j = i + 1; j < data.length; ++j) {
       const point = adjustPoint(getPoint(data[j]), centerOffset, rectangle)
       // The distance from a to be is the same as the distance
       // from b to a, so fill in both entries.
-      m[i][j].distance = m[j][i].distance = calcDistance(boundsCenter, point)
+      m[i][j].distance = m[j][i].distance = calcDistance(rectangleCenter, point)
     }
 
     // The distance from a to a is 0.
