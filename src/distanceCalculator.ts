2ec4d2579c54be23d54db340c86835752cb35808
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-09-07 17:59:14 +0100

    Added doc comments

--- src/distanceCalculator.ts
+++ src/distanceCalculator.ts
@@ -1,14 +1,23 @@
 import { Point } from './types'
 
-export interface DistancePoint {
-  index: number
-  distance: number
-}
-
+/**
+ * Calculate the Manhattan distance between two points.
+ *
+ * @param a A point.
+ * @param b Another point.
+ * @returns The distance between the two points.
+ */
 export function calcManhattanDistance(a: Point, b: Point): number {
   return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
 }
 
+/**
+ * Calculate the Euclidean distance between two points.
+ *
+ * @param a A point
+ * @param b Another point.
+ * @returns The distance between two points.
+ */
 export function calcEuclideanDistance(a: Point, b: Point): number {
   const x = a.x - b.x
   const y = a.y - b.y
