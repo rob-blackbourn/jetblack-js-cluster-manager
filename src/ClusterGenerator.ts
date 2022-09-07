4257b11168b98fea9658f51471c97be89248e5c4
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-09-07 07:37:57 +0100

    Tidy up

--- src/ClusterGenerator.ts
+++ src/ClusterGenerator.ts
@@ -1,12 +1,31 @@
 import { DistancePoint, generateDistanceMatrix } from './distanceMatrices'
 import { Point, Rectangle } from './types'
 
+/**
+ * The cluster generator.
+ *
+ * A cluster holds the distances between all of the points
+ * in a wrap-around world.
+ *
+ * @typeParam T The type of a point.
+ */
 export class ClusterGenerator<T> {
+  /** For each point, the distance to and index of, the other points */
   private distanceMatrix: DistancePoint[][] = []
+  /** The point getter */
   private getPoint: (points: T) => Point
 
+  /** The array of points */
   public points: T[]
 
+  /**
+   * Create a cluster.
+   *
+   * @param points An array of points.
+   * @param getPoint A point getter.
+   * @param rectangle A rectangle that constrains the point.
+   * @param calcDistance A function with calculates the distance between two points.
+   */
   constructor(
     points: T[],
     getPoint: (points: T) => Point,
@@ -23,6 +42,13 @@ export class ClusterGenerator<T> {
     )
   }
 
+  /**
+   * Find points less than or equal to a given distance away.
+   *
+   * @param index The index of the point in the original array.
+   * @param distance The maximum distance.
+   * @returns An array of points.
+   */
   within(index: number, distance: number): T[] {
     return this.distanceMatrix[index]
       .slice(1)
